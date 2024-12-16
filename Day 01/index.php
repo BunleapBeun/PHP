4f7f864b66e884c7a0df69de20b895cddd5eaaba
Bunleap Beun <bunleap997@gmail.com>
2024-12-16 21:18:35 +0700

    add two day

---
+++ Day 01/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day 01</title>
+</head>
+
+<body>
+    <h1>Hello h1</h1>
+    <h2>
+        <?php echo 'Hello h2' ?>
+    </h2>
+
+    <?php
+    echo '<h3>Hello h3</h3>';
+    ?>
+</body>
+
+</html>
