c98566e078bd22b4c0b1d7854f1140ac2ec94ee4
Bunleap Beun <bunleap997@gmail.com>
2024-12-17 00:51:33 +0700

    day7

---
+++ Day 07 - title/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day 07 - title</title>
+</head>
+<body>
+    // day 7 watch video ti 8
+</body>
+</html>
