39082caffca96e3e89d3a6cb963f211f9fd529c9
Bunleap Beun <bunleap997@gmail.com>
2024-12-17 23:35:56 +0700

    update day10

---
+++ Day 10 - Create Database PHP/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day 10 - Create Database PHP</title>
+</head>
+<body>
+    // video 11,
+    // in this lesson we created db in mysql workbench db name reanwebDB
+    // tbl_test, have: id, name, price;
+</body>
+</html>
