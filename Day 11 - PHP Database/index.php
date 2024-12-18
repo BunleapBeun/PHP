fe8dd1a88154079ef6ecb9fa71578acde9754834
Bunleap Beun <bunleap997@gmail.com>
2024-12-18 22:55:55 +0700

    update day11

--- Day 11 - PHP Database/index.php
+++ Day 11 - PHP Database/index.php
@@ -16,16 +16,32 @@
     $password = "Bl123456789";  // MySQL password
     $dbname = "test_db";    // Database name
 
-    try {
-        // Create connection
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // connect with first method
+    $con = new mysqli("localhost", "root", 'Bl123456789', 'test_db');
 
-        // Set PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
+    // connect use mysqli, the second method
+    // $con = new mysqli($servername, $username, $password, $dbname);
+
+    // need fill all column for make it work to put data in the table
+    // $sql = "INSERT INTO tbl_test VALUES(null, 'Apple', 20)";
+    
+    // no need to fill all column but u need to call name of the row correct
+    $sql = "INSERT INTO tbl_test(name, price) VALUES('Mango', 30)";
+    
+    $con->query($sql);
+    // echo $sql;
+
+    // connect to DB use PDO
+    // try {
+    //     // Create connection
+    //     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    //     // Set PDO error mode to exception
+    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     echo "Connected successfully";
+    // } catch (PDOException $e) {
+    //     echo "Connection failed: " . $e->getMessage();
+    // }
     ?>
 
 </body>
